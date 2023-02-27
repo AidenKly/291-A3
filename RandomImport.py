9eff97f8da8b1480239f4c51250f73b49457db37
AidenKly <aklymchu@ualberta.ca>
2023-02-27 14:55:23 -0700

    Changed README to .txt file and started RandomImport.py to randomly insert a certain number of entries into a given database.

---
+++ RandomImport.py
@@ -0,0 +1,13 @@
+import csv
+# Inserts data from a given csv file into a given sql table.
+
+def get_db_info():
+    database_name = input("Enter a Database filename. >> ")
+    table_name = input("Enter the name of the table to be inserted into. >> ")
+    
+    csv_filename = input("Enter a csv filename. >> ")
+    column_map = {}
+    column_map_str = input("Enter the name of a column in the csv file then '':'' then the name of a column in the database table.\n \
+                           Multiple entries are supported if they are seperated by a comma.")
+    
+    ','.split
