8ac2fa25deb8e601fca64139a648c8d1eb74fbd3
AidenKly <aklymchu@ualberta.ca>
2023-02-27 22:02:57 -0700

    Started more of RandomImport

--- RandomImport.py
+++ RandomImport.py
@@ -4,10 +4,17 @@ import csv
 def get_db_info():
     database_name = input("Enter a Database filename. >> ")
     table_name = input("Enter the name of the table to be inserted into. >> ")
-    
+
     csv_filename = input("Enter a csv filename. >> ")
     column_map = {}
     column_map_str = input("Enter the name of a column in the csv file then '':'' then the name of a column in the database table.\n \
                            Multiple entries are supported if they are seperated by a comma.")
     
     ','.split
+
+def read_csv(filename):
+    with open(filename, 'r') as csvfile:
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+        columns = next(csvreader)
+    
