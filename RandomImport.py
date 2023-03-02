75fd01be6f6165255c9894e900dfa7e4b7f2dfab
AidenKly <aklymchu@ualberta.ca>
2023-03-02 15:20:16 -0700

    Added read_csv function. It should work almost completely 👌

--- RandomImport.py
+++ RandomImport.py
@@ -1,4 +1,4 @@
-import csv, sqlite3
+import csv, sqlite3, random
 # Inserts data from a given csv file into a given sql table.
 
 def get_db_info():
@@ -12,9 +12,30 @@ def get_db_info():
     
     ','.split
 
-def read_csv(filename):
+def read_csv(filename, row_count):
+    # Takes in a filename and number of rows to randomly select
+    # Returns a list of randomly selected rows from the specified .csv file name
+    
     with open(filename, 'r') as csvfile:
         # creating a csv reader object
         csvreader = csv.reader(csvfile)
         columns = next(csvreader)
+        rows = []
+        # extracting each data row one by one
+        for row in csvreader:
+            rows.append(row)
+        csvfile.close()
+        
+    row_subset = []
+
+    while len(row_subset) < row_count:
+        random_row = random.choice(rows)
+        if random_row not in row_subset:
+            row_subset.append(random_row)
     
+    return row_subset
+
+def main():
+    print(read_csv("Dataset\\olist_customers_dataset.csv", 5))
+
+main()
