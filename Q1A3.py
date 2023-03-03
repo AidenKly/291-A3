78f4ab9a54c79276cfd488240a76c8c46f68b152
AidenKly <aklymchu@ualberta.ca>
2023-03-03 11:42:36 -0700

    Started question 1

---
+++ Q1A3.py
@@ -0,0 +1,4 @@
+# General prompt:
+# Q1: Given a random customer_postal_code from Customers, find how many orders containing more
+# than 1 item have been placed by customers who have that customer_postal_code.
+
