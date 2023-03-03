c53c8dfd877f9017bbc4cb7053c426be0b437179
AidenKly <aklymchu@ualberta.ca>
2023-03-03 15:29:27 -0700

    Started question 4 and fixed QuestionTemplate

--- Q4A3.py
+++ Q4A3.py
@@ -1,6 +1,10 @@
 --# Q4: Choose a random customer with more than one order and for that customer's orders, find in how
 --# many (unique) postal codes the sellers provided those orders.
 
-SELECT COUNT(DDISTINCT seller_postal_code) 
+SELECT COUNT(DISTINCT seller_postal_code) 
 FROM Customers Cu, Orders Ord, Sellers S, Order_items Oi
-WHERE Cu.customer_id = 
+WHERE Cu.customer_id = Ord.customer_id AND
+Ord.order_id = Oi.order_id AND
+S.seller_id = Oi.seller_id AND
+Cu.customer_id = "{random_customer_id}"
+
